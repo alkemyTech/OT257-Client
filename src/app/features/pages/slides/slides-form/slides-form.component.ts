76a0698852fb5fe97c8b3dc0e5f372fabe6fde05
Gaston <gastoncorrea90e@gmail.com>
2022-07-14 19:54:15 -0300

    add Validations to all form fields

--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -8,9 +8,39 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./slides-form.component.scss']
 })
 export class SlidesFormComponent implements OnInit {
+  // variable to contain property from ckeditor
   public Editor = ClassicEditor;
+  // variable containing the form properties
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) {
+    this.form = this.formBuilder.group({
+      name:['',[Validators.required,Validators.minLength(4)]],
+      description:['',[Validators.required]],
+      order:['',[Validators.required]],
+      image:['',[Validators.required]],
+      userId:['',[]],
+      create_at:['',[]],
+      update_at:['',[]],
+      delete_at:['',[]]
+    })
+   }
+
+   get Name(){
+    return this.form.get('name');
+   }
+
+   get Description(){
+    return this.form.get('description');
+   }
+
+   get Order(){
+    return this.form.get('order');
+   }
+
+   get Image(){
+    return this.form.get('image');
+   }
 
   ngOnInit(): void {
   }
