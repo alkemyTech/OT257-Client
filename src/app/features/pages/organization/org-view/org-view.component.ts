f47a69fc5538d790356d1b021921bfead0353bbd
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-07-15 16:00:40 -0300

    Prettier

--- src/app/features/pages/organization/org-view/org-view.component.ts
+++ src/app/features/pages/organization/org-view/org-view.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { OrgViewService } from '../../../../core/services/org-view.service';
-import { Organization,Data } from 'src/app/core/models/IOrganization';
+import { Component, OnInit } from "@angular/core";
+import { OrgViewService } from "../../../../core/services/org-view.service";
+import { Organization, Data } from "src/app/core/models/IOrganization";
 
 @Component({
-  selector: 'app-org-view',
-  templateUrl: './org-view.component.html',
-  styleUrls: ['./org-view.component.scss']
+  selector: "app-org-view",
+  templateUrl: "./org-view.component.html",
+  styleUrls: ["./org-view.component.scss"],
 })
 export class OrgViewComponent implements OnInit {
-
   public organizationData!: Data;
 
-  constructor(private orgService: OrgViewService) { }
+  constructor(private orgService: OrgViewService) {}
 
   ngOnInit(): void {
     this.getOrganizationView();
@@ -21,15 +20,8 @@ export class OrgViewComponent implements OnInit {
     this.orgService.getorganizationData().subscribe({
       next: (response: Organization) => {
         this.organizationData = response.data;
-        
       },
-      error: (err) => {
-      }
+      error: (err) => {},
     });
-
-    }
-      
-   
   }
-
-
+}
