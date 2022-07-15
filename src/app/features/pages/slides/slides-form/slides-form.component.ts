f1d0aaacbbfae8d8857ada516162559bff66aaa9
Gaston <gastoncorrea90e@gmail.com>
2022-07-15 14:35:03 -0300

    add function submit on form slide and add function for change image to base64

--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { HelpersService } from 'src/app/core/services/helpers.service';
 
 @Component({
   selector: 'app-slides-form',
@@ -13,7 +14,7 @@ export class SlidesFormComponent implements OnInit {
   // variable containing the form properties
   form: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) {
+  constructor(private formBuilder:FormBuilder,private helpers:HelpersService) {
     this.form = this.formBuilder.group({
       name:['',[Validators.required,Validators.minLength(4)]],
       description:['',[Validators.required]],
@@ -43,6 +44,30 @@ export class SlidesFormComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  fileChange(e:Event){
+    e.preventDefault();
+    console.log("valor del evento cuando se selecciona un archivo");
+    console.log(e);
+    if(!this.helpers.fileExtensionCheck(e)){
+      this.form.controls['image'].setErrors({'incorrect':true});
+      console.log("valor del input image");
+      console.log(this.form.get('image'));
+    }else{
+      
+    }
+  }
+
+  sendForm(){
+    console.log("Valor del form cuando se presiona guardar");
+    console.log(this.form.value);
+    if(this.form.valid){
+
+    }else{
+      this.form.markAllAsTouched();
+    }
+
   }
 
 }
