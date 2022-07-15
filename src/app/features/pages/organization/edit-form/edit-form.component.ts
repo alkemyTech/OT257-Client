567f343de668a44b93e9e6e1b4006b3957b68fa7
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-07-15 19:02:43 -0300

    add helpers service

--- src/app/features/pages/organization/edit-form/edit-form.component.ts
+++ src/app/features/pages/organization/edit-form/edit-form.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { HelpersService } from "src/app/core/services/helpers.service";
 
 @Component({
-  selector: 'app-edit-form',
-  templateUrl: './edit-form.component.html',
-  styleUrls: ['./edit-form.component.scss']
+  selector: "app-edit-form",
+  templateUrl: "./edit-form.component.html",
+  styleUrls: ["./edit-form.component.scss"],
 })
 export class EditFormComponent implements OnInit {
-
   Editor = ClassicEditor;
   form!: FormGroup;
   event!: any;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private helpers: HelpersService) { }
 
   ngOnInit(): void {
     this.editForm();
@@ -21,14 +21,46 @@ export class EditFormComponent implements OnInit {
 
   editForm() {
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(4)]],
-      logo: ['', [Validators.required]],
-      longDescription: ['', [Validators.required, Validators.minLength(20)]],
-      shortDescription: ['', [Validators.required, Validators.minLength(10)]],
-      facebook: ['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
-      linkedin: ['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
-      instagram: ['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
-      twitter: ['', [Validators.required, Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]]
+      name: ["", [Validators.required, Validators.minLength(4)]],
+      logo: ["", [Validators.required]],
+      longDescription: ["", [Validators.required, Validators.minLength(20)]],
+      shortDescription: ["", [Validators.required, Validators.minLength(10)]],
+      facebook: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+          ),
+        ],
+      ],
+      linkedin: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+          ),
+        ],
+      ],
+      instagram: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+          ),
+        ],
+      ],
+      twitter: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?"
+          ),
+        ],
+      ],
     });
   }
 
@@ -40,23 +72,17 @@ export class EditFormComponent implements OnInit {
     this.event = event;
   }
 
-
   onSubmit() {
+    if (!this.helpers.fileExtensionCheck(this.event)) {
+      this.form.controls["logo"].setErrors({ 'incorrect': true });
 
+      console.log(this.form.value);
 
-
-
-    console.log(this.form.value);
-
-
-    if (this.form.invalid) {
-      return Object.values(this.form.controls).forEach(control => {
-        control.markAsTouched();
+      if (this.form.invalid) {
+        return Object.values(this.form.controls).forEach((control) => {
+          control.markAsTouched();
+        });
       }
-      );
     }
   }
-
-
-
 }
