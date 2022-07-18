4382a69a4c75d3f05dd23d8c65edfc2d028a20f0
Gaston <gastoncorrea90e@gmail.com>
2022-07-18 16:36:42 -0300

    Corrections error on fileExtensionCheck function

--- src/app/core/services/helpers.service.ts
+++ src/app/core/services/helpers.service.ts
@@ -11,21 +11,6 @@ export class HelpersService {
   fileExtensionCheck( event: any ) {
     const extensionFile = event.target.files[0].type;
 
-    if ( extensionFile === 'image/jpg' || extensionFile === 'image/png' ) {
-      return true
-    } else {
-      return false
-    }
+    return extensionFile === 'image/jpg' || extensionFile === 'image/png' ;
   }
-
-  // convert file image to base64
-  convertFileToBase64(file:any):Observable<string>{
-    const result = new ReplaySubject<string>(1);
-    const reader = new FileReader();
-
-    reader.readAsBinaryString(file.target.files[0]);
-    reader.onload = (event) => result.next(btoa(event.target?.result?.toString() ?? ''));
-    return result;
-  }
-
 }
