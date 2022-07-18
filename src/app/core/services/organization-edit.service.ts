16b617ba9f56a8c6caf4650b8896074b2d22633d
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-07-18 16:38:17 -0300

    delete typings.d.ts

--- src/app/core/services/organization-edit.service.ts
+++ src/app/core/services/organization-edit.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Organization, Data } from 'src/app/core/models/IOrganization';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrganizationEditService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getorganizationData(): Observable<any> {
+    return this.http.get('https://ongapi.alkemy.org/api/organization');
+  }
+
+  public getOrganizationDataById(id: string): Observable<any> {
+    return this.http.get(`https://ongapi.alkemy.org/api/organization/${id}`);
+  }
+
+  public editOrganization(id: string, data: Data): Observable<any> {
+    return this.http.put(`https://ongapi.alkemy.org/api/organization/${id}`, data);
+  }
 }
