9bb1a280dd0c276832c39b910a3aed9ed40f4136
Gaston <gastoncorrea90e@gmail.com>
2022-07-17 23:51:07 -0300

    Add component title

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -12,6 +12,8 @@ import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AuthFormComponent } from './pages/auth/auth-form/auth-form.component';
+import { TituloComponent } from './pages/titulos/titulo/titulo.component';
+import { OrgViewComponent } from "./pages/organization/org-view/org-view.component";
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { AuthFormComponent } from './pages/auth/auth-form/auth-form.component';
     TestimonialFormComponent,
     UserFormComponent,
     AuthFormComponent,
+    TituloComponent,
+    OrgViewComponent
   ],
   exports: [
     ActivityFormComponent,
