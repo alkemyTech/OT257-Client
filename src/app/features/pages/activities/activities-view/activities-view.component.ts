aa38a4c42fed472a415d849abb4f16170cc7ead4
Gaston <gastoncorrea90e@gmail.com>
2022-07-21 18:25:54 -0300

    fixed activities component in module

---
+++ src/app/features/pages/activities/activities-view/activities-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-view',
+  templateUrl: './activities-view.component.html',
+  styleUrls: ['./activities-view.component.scss']
+})
+export class ActivitiesViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
