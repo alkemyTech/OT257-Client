889cf664dfd3f9dac50554b80d9df11ef397ebb9
karina.martinez <martinezjerezkarina@gmail.com>
2022-07-24 15:30:53 -0500

    Creación de método reutilizable para petición PUT

--- src/app/core/services/private-api-service.service.ts
+++ src/app/core/services/private-api-service.service.ts
@@ -1,42 +1,71 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PrivateApiServiceService {
 
-
   private httpHeaders = {
     headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-    })
-};
-  constructor(private http: HttpClient) { 
+      "Content-Type": "application/json",
+    }),
+  };
+  constructor(private http: HttpClient) {}
 
+  /**
+   * Send a POST request
+   * @param url
+   * @param data
+   * @returns {Promise<Object>}
+   */
+
+  public async sendPostRequest(url: any, data: any) {
+    return this.http
+      .post(url, data, this.httpHeaders)
+      .pipe(
+        tap({
+          error: (error) => {
+            if (error.status === 500) {
+              // Handle 500
+            } else if (error.status === 400) {
+              // Handle 400
+            } else if (error.status === 401) {
+              // Handle 401
+            }
+          },
+        })
+      )
+      .toPromise();
   }
 
-/**
-     * Send a POST request
-     * @param url
-     * @param data
-     * @returns {Promise<Object>}
-     */
-
- public async sendPostRequest(url:any, data:any){
-  return this.http.post(url, data, this.httpHeaders).pipe(
-      tap({
-          error: error => {
-              if (error.status === 500) {
-                  // Handle 500
-              } else if (error.status === 400) {
-                  // Handle 400
-              } else if (error.status === 401) {
-                  // Handle 401
-              }
-          }})).toPromise();
-}
-
-
+  /**
+   * send put request
+   * 
+   * @param url 
+   * @param id 
+   * @param data 
+   * @returns {Promise<Object>}
+   */
+  public async sendPutRequest(url: string, id: number, data: any): Promise<any> {
+    return this.http
+      .put(`${url}/${id}`, data, this.httpHeaders)
+      .pipe(
+        tap({
+          error: (error) => {
+            if (error.status === 500) {
+                console.log("Error 500:: ");
+              // Handle 500
+            } else if (error.status === 400) {
+                console.log("Error 400:: ");
+              // Handle 400
+            } else if (error.status === 401) {
+                console.log("Error 401:: ");
+              // Handle 401
+            }
+          },
+        })
+      ).toPromise();
+  }
 }
