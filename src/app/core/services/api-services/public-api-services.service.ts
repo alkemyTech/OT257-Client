46eec55ca905f4485a25e094382fd408c0df0bbd
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-07-25 11:53:16 -0300

    add sendPostRequest function

--- src/app/core/services/api-services/public-api-services.service.ts
+++ src/app/core/services/api-services/public-api-services.service.ts
@@ -3,25 +3,25 @@ import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PublicApiServicesService {
 
-  private httpHeaders = {
-    headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-    })
-};
+    private httpHeaders = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+        })
+    };
 
-  constructor( private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  /**
-     * Send a GET request
-     * @param url
-     * @returns {Promise<Object>}
-     */
-    public async sendGetRequest(url:any, id:any): Promise<any>{
-        return this.http.get(url+'/'+id, this.httpHeaders).pipe(
+    /**
+       * Send a GET request
+       * @param url
+       * @returns {Promise<Object>}
+       */
+    public async sendGetRequest(url: any, id: any): Promise<any> {
+        return this.http.get(url + '/' + id, this.httpHeaders).pipe(
             tap({
                 error: error => {
                     if (error.status === 500) {
@@ -31,6 +31,22 @@ export class PublicApiServicesService {
                     } else if (error.status === 401) {
                         // Handle 401
                     }
-                }})).toPromise();
+                }
+            })).toPromise();
+    }
+
+    public async sendPostRequest(url: any, data: any): Promise<any> {
+        return this.http.post(url, data, this.httpHeaders).pipe(
+            tap({
+                error: error => {
+                    if (error.status === 500) {
+                        // Handle 500
+                    } else if (error.status === 400) {
+                        // Handle 400
+                    } else if (error.status === 401) {
+                        // Handle 401
+                    }
+                }
+            })).toPromise();
     }
 }
