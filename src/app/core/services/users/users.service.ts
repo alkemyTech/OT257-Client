3945922e7c1fe1c059aeec205391e461f9b86237
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-07-26 14:00:23 -0300

    add CRUD function to service

--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,16 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { User } from "../../models/user.model";
+import { PrivateApiServiceService } from "../private-api-service.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class UsersService {
-
-  constructor(private http: HttpClient) { }
-
-  getUsersFromApi(): Observable<any> {
-    return this.http.get('https://ongapi.alkemy.org/api/users');
+export class UsersService extends PrivateApiServiceService {
+  url = "https://ongapi.alkemy.org/api/users";
+  constructor(private httpClient: HttpClient) {
+    super(httpClient);
   }
 
+  getUsers() {
+    return this.sendGetRequest(`${this.url}`);
+  }
+
+  createUser(dataUser: User) {
+    return this.sendPostRequest(`${this.url}`, dataUser);
+  }
+
+  getUser(id: string) {
+    return this.sendGetRequest(`${this.url}`, id);
+  }
+
+  updateUser(id: any, dataUser: User) {
+    dataUser["id"] = id;
+    return this.sendPutRequest(`${this.url}`, id, dataUser);
+  }
+
+  deleteUser(id: string) {
+    return this.sendDeleteRequest(`${this.url}`, id);
+  }
 }
