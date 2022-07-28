f621f52dbb4cccce1e8a2e5ddbbf29efc6a5e891
Gaston <gastoncorrea90e@gmail.com>
2022-07-28 15:29:38 -0300

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  URLSLIDES : "https://ongapi.alkemy.org/api/slides"
 };
--- src/environments/environment.slides.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production : false,
-    URL : "https://ongapi.alkemy.org/api"
-}
