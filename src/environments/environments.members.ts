27dc5416cb3e87176f9f74ba5589f3be9248473b
Wilson Lasso <wilago1979@gmail.com>
2022-07-28 13:57:10 -0500

    Start ignoring enrironments 5

--- src/environments/environments.members.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    urlMembers:"https://ongapi.alkemy.org/api/members",
-    
-  };
-  
--- src/environments/environments.news.ts
+++
@@ -1,7 +0,0 @@
-
-export const environment = {
-    production: false,
-    urlNews:"https://ongapi.alkemy.org/api/news",
-    
-  };
-  
