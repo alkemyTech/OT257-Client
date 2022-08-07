395fce41697603d7979fcc666971783094ef6c79
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-08-07 16:17:37 -0300

    add images to asset

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -10,7 +10,25 @@ import { Slides } from '../../../core/models/slides.model';
 export class CarouselComponent implements OnInit {
   @ViewChild('carousel') _carousel!: ElementRef;
 
- @Input() slides: Slides[] = [];
+  @Input() slides: Slides[] = [];
+
+  slidesStatic = [
+    {
+      name: 'De vuelta al cole!',
+      description: 'La falta de escuelas en las  comunidades y la alta rotación de docentes calificados  hace que las chicas y chicos tengan que trasladarse a otras localidades y recorrer muchos kilómetros por día.',
+      image: 'assets/images/campania-escolar1.jpg'
+    },
+    {
+      name: 'De vuelta al cole!',
+      description: 'La falta de escuelas en las  comunidades y la alta rotación de docentes calificados  hace que las chicas y chicos tengan que trasladarse a otras localidades y recorrer muchos kilómetros por día.',
+      image: 'assets/images/campania-escolar2.jpg'
+    },
+    {
+      name: 'De vuelta al cole!',
+      description: 'La falta de escuelas en las  comunidades y la alta rotación de docentes calificados  hace que las chicas y chicos tengan que trasladarse a otras localidades y recorrer muchos kilómetros por día.',
+      image: 'assets/images/campania-escolar3.jpg'
+    }
+  ]
 
   constructor() { }
 
