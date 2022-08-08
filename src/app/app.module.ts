3a940f63640db98129eb1e76347b051f084f0991
Wilson Lasso <wilago1979@gmail.com>
2022-08-08 14:22:57 -0500

    Solution PR 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
-
-
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { ROOT_REDUCERS } from './state/app.state';
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { ROOT_REDUCERS } from "./state/app.state";
 import { AuthEffects } from "./state/effects/auth.effects";
-import { NewsEffects } from './state/effects/news.effects';
+import { NewsEffects } from "./state/effects/news.effects";
 import { FeaturesModule } from "./features/features.module";
 import { CoreModule } from "./core/core.module";
 import { BrowserModule } from "@angular/platform-browser";
@@ -21,8 +19,8 @@ import { RouterModule } from "@angular/router";
     CoreModule,
     FeaturesModule,
     StoreModule.forRoot(ROOT_REDUCERS),
-    EffectsModule.forRoot([AuthEffects,NewsEffects]),
-    StoreDevtoolsModule.instrument({ }),
+    EffectsModule.forRoot([AuthEffects, NewsEffects]),
+    StoreDevtoolsModule.instrument({}),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,18 +1,15 @@
-import { ActionReducerMap} from "@ngrx/store";
+import { ActionReducerMap } from "@ngrx/store";
 import { AuthState } from "../core/models/auth.model";
 import { NewsState } from "../core/models/new.model";
 import { authReducer } from "./reducers/auth.reducer";
 import { newsReducer } from "./reducers/news.reducers";
 
-
-
 export interface AppState {
-    auth: AuthState;
-    data: NewsState;
+  auth: AuthState;
+  data: NewsState;
 }
-
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
-    auth: authReducer,
-    data: newsReducer
-}
+  auth: authReducer,
+  data: newsReducer,
+};
