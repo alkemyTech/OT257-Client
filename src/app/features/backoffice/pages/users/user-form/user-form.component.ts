e01f57e3c470d5a85ed7e925c77fb9f131a606f5
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-08-10 14:43:07 -0300

    add format

--- src/app/features/backoffice/pages/users/user-form/user-form.component.ts
+++ src/app/features/backoffice/pages/users/user-form/user-form.component.ts
@@ -1,30 +1,61 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
-import { Address } from 'ngx-google-places-autocomplete/objects/address';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { GooglePlaceDirective } from "ngx-google-places-autocomplete";
+import { Address } from "ngx-google-places-autocomplete/objects/address";
 
 @Component({
-  selector: 'app-user-form',
-  templateUrl: './user-form.component.html',
-  styleUrls: ['./user-form.component.scss']
+  selector: "app-user-form",
+  templateUrl: "./user-form.component.html",
+  styleUrls: ["./user-form.component.scss"],
 })
 export class UserFormComponent implements OnInit {
   @ViewChild("placesRef") placesRef!: GooglePlaceDirective;
 
   options: any = {
     types: [],
-    componentRestrictions: { country: 'AR' }
+    componentRestrictions: { country: "AR" },
   };
   title!: string;
   latitude!: number;
   longitude!: number;
+  center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
+  zoom = 4;
+  display: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.setCurrentPosition();
   }
 
-  public handleAddressChange(address: Address) {
-    // Do some stuff
+  handleAddressChange(address: Address) {
+    this.latitude = address.geometry.location.lat();
+    this.longitude = address.geometry.location.lng();
+    this.center = {
+      lat: this.latitude,
+      lng: this.longitude,
+    };
+    this.zoom = 15;
   }
 
+  public setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.zoom = 15;
+      });
+    }
+  }
+
+  moveMap(event: google.maps.MapMouseEvent | any) {
+    this.center = event.latLng.toJSON();
+  }
+
+  move(event: google.maps.MapMouseEvent | any) {
+    this.display = event.latLng.toJSON();
+  }
 }
