b91e6d5819c95d5a136bab020d8d63e9a2a076b9
Gaston <gastoncorrea90e@gmail.com>
2022-08-09 21:47:17 -0300

    Fomatter header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,42 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLinkActive} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  menuList = [{
-    'name':'Inicio',
-    'link' : '',
-    'usuarioLog': false
-  },
-  {
-    'name':'Nosotros',
-    'link' : '/nosotros',
-    'usuarioLog': false
-  },
-  {
-    'name':'Contacto',
-    'link' : '/contact-form',
-    'usuarioLog': false
-  },
-  {
-    'name':'Donaciones de utiles escolares',
-    'link' : '#',
-    'usuarioLog': false
-  },
-  {
-    'name':'Donaciones de juguetes',
-    'link' : '#',
-    'usuarioLog': false
-  }
+  menuList = [
+    {
+      name: "Inicio",
+      link: "",
+      usuarioLog: false,
+    },
+    {
+      name: "Nosotros",
+      link: "/nosotros",
+      usuarioLog: false,
+    },
+    {
+      name: "Contacto",
+      link: "/contact-form",
+      usuarioLog: false,
+    },
+    {
+      name: "Donaciones de utiles escolares",
+      link: "#",
+      usuarioLog: false,
+    },
+    {
+      name: "Donaciones de juguetes",
+      link: "#",
+      usuarioLog: false,
+    },
   ];
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
