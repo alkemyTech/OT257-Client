03061a3af480ae66f72a4bb42d58ff4195e618a0
Wilson Lasso <wilago1979@gmail.com>
2022-08-11 10:09:32 -0500

    Use footer

--- src/app/features/public/pages/langings/juguetes/juguetes.component.spec.ts
+++ src/app/features/public/pages/langings/juguetes/juguetes.component.spec.ts
@@ -1,16 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { JuguetesComponent } from './juguetes.component';
+import { JuguetesComponent } from "./juguetes.component";
 
-describe('JuguetesComponent', () => {
+describe("JuguetesComponent", () => {
   let component: JuguetesComponent;
   let fixture: ComponentFixture<JuguetesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ JuguetesComponent ]
-    })
-    .compileComponents();
+      declarations: [JuguetesComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('JuguetesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
