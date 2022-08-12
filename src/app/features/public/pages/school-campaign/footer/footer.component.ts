75737e4e5baf7ee2675a274bb3ae0d9dbcc75e02
Gaston <gastoncorrea90e@gmail.com>
2022-08-11 22:26:06 -0300

    add media queries to footer from school-campaign

--- src/app/features/public/pages/school-campaign/footer/footer.component.ts
+++ src/app/features/public/pages/school-campaign/footer/footer.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Data } from "src/app/core/models/IOrganization";
+import { OrgViewService } from "src/app/core/services/org-view.service";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.scss"],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  orgData!: Data;
+  constructor(private orgSvc: OrgViewService) {}
 
   ngOnInit(): void {
+    this.orgSvc
+      .getorganizationData()
+      .pipe()
+      .subscribe((res) => {
+        this.orgData = res.data;
+      });
   }
-
 }
