13bcc3c187853aa881260295bcc2f4ab81dacf58
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-08-15 14:17:44 -0300

    add debounce to search users names bar

--- src/app/features/backoffice/pages/users/users-list/users-list.component.ts
+++ src/app/features/backoffice/pages/users/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
 import { loadUsers, deleteUser } from "src/app/state/actions/users.action";
 import { AppState } from "src/app/state/app.state";
 import {
@@ -16,6 +17,8 @@ import {
 export class UsersListComponent implements OnInit {
   loading$: Observable<boolean> = new Observable();
   users$: Observable<any> = new Observable();
+  private subjectKeyUp = new Subject<any>();
+  userFilter: any = [];
 
   constructor(private store: Store<AppState>) { }
 
@@ -32,8 +35,26 @@ export class UsersListComponent implements OnInit {
   }
 
   searchUser($event: any) {
-    const value = $event.target.value;
-    console.log(value);
-
+    $event.preventDefault();
+    this.subjectKeyUp.next($event.target.value);
+    this.subjectKeyUp.pipe(debounceTime(500)).subscribe((inputValue) => {
+      this.userFilter = [];
+      if (inputValue.length > 2) {
+        this.userFilter = [];
+        this.users$.subscribe((data) => {
+          for (let dataName of data) {
+            let nameSlide = dataName.name.substring(0, inputValue.length);
+            if (nameSlide.toLowerCase() == inputValue.toLowerCase()) {
+              this.userFilter.push(dataName);
+            }
+          }
+          if (this.userFilter.length === 0) {
+            this.userFilter = data;
+          }
+        });
+      }
+    });
   }
+
+
 }
