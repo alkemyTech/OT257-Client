d309b59a6a459627bedb74bc221f4456803beb43
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-08-15 18:24:21 -0300

    add canActivate to Public Routing Module

--- src/app/features/public/public-routing.module.ts
+++ src/app/features/public/public-routing.module.ts
@@ -5,6 +5,7 @@ import { AboutComponent } from "./pages/about/page/about.component";
 import { HomeComponent } from "./pages/home/page/home.component";
 import { ContactFormComponent } from "./pages/contact-form/contact-form.component";
 import { FooterComponent } from "./pages/school-campaign/footer/footer.component";
+import { LoginGuard } from "src/app/shared/guards/login.guard";
 
 export const routes: Routes = [
   {
@@ -25,6 +26,7 @@ export const routes: Routes = [
       import("./pages/auth/login-form/login-form-routing.module").then(
         (m) => m.LoginFormRoutingModule
       ),
+    canActivate: [LoginGuard],
   },
   {
     path: "registro",
