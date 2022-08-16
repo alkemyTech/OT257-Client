2276aab163f1573540696e968d85f664839ee23c
Gaston <gastoncorrea90e@gmail.com>
2022-08-16 19:43:34 -0300

    formatter guards components

--- src/app/shared/guards/backoffice.guard.ts
+++ src/app/shared/guards/backoffice.guard.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AppState } from "src/app/state/app.state";
-import { Store } from "@ngrx/store";
-import { selectIsAuthenticated } from "src/app/state/selectors/auth.selectors";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackofficeGuard implements CanActivate {
 
-  constructor(private store: Store<AppState>, private router: Router) {}
+  constructor( private router: Router) {}
   valor = localStorage.getItem('token')
   canActivate(
     route: ActivatedRouteSnapshot,
