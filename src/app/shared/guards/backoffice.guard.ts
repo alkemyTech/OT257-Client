877be78a63cd0fe76623879ba6c3246b602d3d5b
Gaston <gastoncorrea90e@gmail.com>
2022-08-16 19:43:44 -0300

    formatter guards components

--- src/app/shared/guards/backoffice.guard.ts
+++ src/app/shared/guards/backoffice.guard.ts
@@ -1,24 +1,32 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class BackofficeGuard implements CanActivate {
-
-  constructor( private router: Router) {}
-  valor = localStorage.getItem('token')
+  constructor(private router: Router) {}
+  valor = localStorage.getItem("token");
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        if(this.valor){
-          return true;
-        }else{
-          this.router.navigate(['/iniciar-sesion']);
-          return false
-        }
-      }
-  
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.valor) {
+      return true;
+    } else {
+      this.router.navigate(["/iniciar-sesion"]);
+      return false;
+    }
+  }
 }
